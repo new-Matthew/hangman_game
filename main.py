b6abb849fb6d1e487fbcf2c502af36b4610a4779
matheus <matheusrodrig16@gmail.com>
2024-11-17 12:58:46 -0300

    feat: implement placeholder and display logic for correct or wrong guesses

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-
-
 from hangman_words import word_list
 import random
 
 chosen_word = random.choice(word_list)
+size_chosen_word = len(chosen_word)
+
 print(chosen_word)
 guess = input("Escolha uma letra: ").lower()
 
-for char in chosen_word:
-    if char == guess:
-        print("Right")
-    else:
-        print("Wrong")
+placeholder = ""
 
-print(guess)
+for position in  range(size_chosen_word):
+    placeholder += "_ "
+
+print(placeholder)
+
+display = ""
+
+for letter in chosen_word:
+    if letter == guess:
+        display += letter + " "
+    else:
+        display += "_ "
+
+print(display)
+
 
 
