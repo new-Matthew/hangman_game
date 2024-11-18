9df3e0df315103a7ef2be48934558bde02c57ad1
matheus <matheusrodrig16@gmail.com>
2024-11-18 18:23:19 -0300

    Refactor: Improve user feedback

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from hangman_words import word_list
 from hangman_arts import stages
+from hangman_arts import logo
 import random
 
+print(logo)
 chosen_word = random.choice(word_list)
 size_chosen_word = len(chosen_word)
 lives = 6
+
 placeholder = ""
 
 for position in range(size_chosen_word):
@@ -20,14 +23,16 @@ while not game_over:
     display = ""
     guess = input("Escolha uma letra: ").lower()
 
+    if guess in correct_guesses:
+        print(f"A letra {guess} já foi escolhida")
+
     for letter in chosen_word:
         if letter == guess:
             display += letter
             correct_guesses.append(guess)
 
-
         elif letter in correct_guesses:
-            display += letter   
+            display += letter  
 
         else:
             display += "_"
@@ -35,16 +40,16 @@ while not game_over:
     if guess not in chosen_word:
         lives -= 1
 
-    print(lives)
-    print(stages[lives])
+    print(f"************************RESTAM {lives} VIDAS!************************")
     print(display)
+    print(stages[lives])
 
     if lives == 0:
-        print("Você perdeu!")
+        print("************************Que pena, Você perdeu!************************")
+        print(f"A palavra era: {chosen_word}")
         game_over = True
 
     if display == chosen_word:
         game_over = True
-        print("você venceu!!")
+        print("************************PARABÉNS VOCÊ VENCEU!!************************")
  
-
